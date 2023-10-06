3974d7618802739241b9a9ecab0e58ac24831415
guillaume maiano <guillaume.maiano.external@worldline.com>
2023-10-06 11:09:20 +0200

    Initial Commit

---
+++ sdk-questions/sdk_questions.h
@@ -0,0 +1,18 @@
+//
+//  sdk_questions.h
+//  sdk-questions
+//
+//  Created by Guillaume Maiano on 06/10/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sdk_questions.
+FOUNDATION_EXPORT double sdk_questionsVersionNumber;
+
+//! Project version string for sdk_questions.
+FOUNDATION_EXPORT const unsigned char sdk_questionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk_questions/PublicHeader.h>
+
+
